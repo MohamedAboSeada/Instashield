cee7a35cc28b5784b404c434e345375d86342bbb
MohamedAboSeada <madoaxel287@gmail.com>
2026-05-10 20:02:43 +0300

    feat: create strings directory to save hardcoded app strings and keep it consistent.
    - Standardize app text and storage key constants.
    - Keep API route strings centralized for consistent endpoint usage.
    - create app_strings barrel file to keep imports organized.

---
+++ lib/core/strings/storage_keys.dart
@@ -0,0 +1,5 @@
+abstract final class StorageKeys {
+  static const String jwtToken = 'user-token';
+  static const String jwtExpiryDate = 'token-expiry-date';
+  static const String sessionUser = 'session-user';
+}
