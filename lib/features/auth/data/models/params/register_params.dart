a13870de32665769d59bba1f9c7d3e016c2841a3
MohamedAboSeada <madoaxel287@gmail.com>
2026-05-11 20:46:55 +0300

    refactor: remove unused parameters from RegisterParams class (Phone Number, National ID)

--- lib/features/auth/data/models/params/register_params.dart
+++ lib/features/auth/data/models/params/register_params.dart
@@ -1,25 +1,19 @@
 class RegisterParams {
   final String email;
-  final String phoneNumber;
   final String fullName;
   final String password;
-  final String nationalId;
 
   RegisterParams({
     required this.email,
-    required this.phoneNumber,
     required this.fullName,
     required this.password,
-    required this.nationalId,
   });
 
   Map<String, Object?> toMap() {
     return <String, Object?>{
       'email': email,
-      'phoneNumber': phoneNumber,
       'fullName': fullName,
       'password': password,
-      'nationalId': nationalId,
     };
   }
 }
@@ -31,10 +25,8 @@ class RegisterMerchantParams extends RegisterParams {
 
   RegisterMerchantParams({
     required super.email,
-    required super.phoneNumber,
     required super.fullName,
     required super.password,
-    required super.nationalId,
     required this.merchantName,
     required this.commercialRegistration,
     required this.taxNumber,
