13b1dbed369d49fe9f80639bbc8de0b1e2fd10ff
MohamedAboSeada <madoaxel287@gmail.com>
2026-05-14 16:14:38 +0300

    style: enhance reset_success_screen style and update it's screenshot in markdown.

--- lib/features/auth/presentation/view/pages/reset_success_page.dart
+++ lib/features/auth/presentation/view/pages/reset_success_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:heroicons/heroicons.dart';
 
+import '../../../../../core/extensions/context_extensions.dart';
 import '../../../../../core/forms/form_button.dart';
 import '../../../../../core/router/app_routes.dart';
 import '../widgets/auth_header.dart';
@@ -13,25 +14,91 @@ class ResetSuccessPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 24.0.w),
-        width: double.infinity,
-        child: Column(
-          crossAxisAlignment: .stretch,
-          mainAxisAlignment: .center,
-          children: [
-            AuthHeader(
-              title: "Password Updated",
-              subtitle:
-                  "Your password has been changed successfully, Use your new password to login to your account.",
-              icon: HeroIcon(.checkBadge, size: 32.0.r),
-            ),
-            40.0.verticalSpace,
-            FormButton(
-              label: "Back to login",
-              onPressed: () => context.go(AppRoutes.loginPath),
-            ),
-          ],
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 24.0, horizontal: 24.0.w),
+          width: double.infinity,
+          child: Column(
+            crossAxisAlignment: .stretch,
+            mainAxisAlignment: .center,
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    AuthHeader(
+                      title: "Password Updated",
+                      subtitle:
+                          "Your password has been changed successfully, Use your new password to login.",
+                      icon: HeroIcon(.checkBadge, size: 32.0.r),
+                    ),
+                    32.0.verticalSpace,
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 4.0.w,
+                        vertical: 12.0.h,
+                      ),
+                      decoration: BoxDecoration(
+                        color: context.colorScheme.surfaceContainer,
+                        borderRadius: .circular(12.0.r),
+                        border: BoxBorder.all(
+                          color: context.colorScheme.outline.withValues(
+                            alpha: 0.2,
+                          ),
+                        ),
+                      ),
+                      child: Column(
+                        children: [
+                          _buildSecurityItem(
+                            HeroIcons.shieldCheck,
+                            "Session Secured",
+                            "For your protection you were logged out to keep your financial data safe.",
+                          ),
+                          const Divider(),
+                          _buildSecurityItem(
+                            HeroIcons.fingerPrint,
+                            "Biometric disabled",
+                            "You will need to re-enable Fingerprint or Face ID after your next login.",
+                          ),
+                          const Divider(),
+                          _buildSecurityItem(
+                            HeroIcons.lockClosed,
+                            "End-to-end Encryption",
+                            "Your bank accounts and transfers are secured with bank-grade encryption.",
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              FormButton(
+                label: "Back to login",
+                onPressed: () => context.go(AppRoutes.loginPath),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildSecurityItem(HeroIcons icon, String title, String subtitle) {
+    return Builder(
+      builder: (context) => ListTile(
+        leading: CircleAvatar(child: HeroIcon(icon)),
+        titleAlignment: ListTileTitleAlignment.top,
+        title: Text(
+          title,
+          style: context.textTheme.titleMedium?.copyWith(
+            color: context.colorScheme.primary,
+            fontWeight: .bold,
+          ),
+        ),
+        subtitle: Text(
+          subtitle,
+          style: context.textTheme.bodyMedium?.copyWith(
+            color: context.colorScheme.onSurface.withValues(alpha: 0.6),
+          ),
         ),
       ),
     );
